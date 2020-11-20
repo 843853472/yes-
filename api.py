5c5a3f5823a30ce57ac318a1eca8697658bb35d0
franjenk <843853472@qq.com>
2020-11-20 16:21:01 +0800

    适配Ubuntu

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from scipy.spatial import distance as dist
+from scipy import spatial
 import PIL.Image
 import dlib
 from PIL import ImageFile
@@ -29,12 +29,12 @@ face_encoder = dlib.face_recognition_model_v1(face_recognition_model)
 def eye_aspect_ratio(eye):
 	# compute the euclidean distances between the two sets of
 	# vertical eye landmarks (x, y)-coordinates
-	A = dist.euclidean(eye[1], eye[5])
-	B = dist.euclidean(eye[2], eye[4])
+	A = spatial.distance.euclidean(eye[1], eye[5])
+	B = spatial.distance.euclidean(eye[2], eye[4])
 
 	# compute the euclidean distance between the horizontal
 	# eye landmark (x, y)-coordinates
-	C = dist.euclidean(eye[0], eye[3])
+	C = spatial.distance.euclidean(eye[0], eye[3])
 
 	# compute the eye aspect ratio
 	ear = (A + B) / (2.0 * C)
