ff696ab99636780673c55f838cdf1be2d88d6597
franjenk <843853472@qq.com>
2020-11-21 21:03:17 +0800

    删注释

--- api.py
+++ api.py
@@ -24,64 +24,25 @@ cnn_face_detector = dlib.cnn_face_detection_model_v1(cnn_face_detection_model)
 face_recognition_model = models.face_recognition_model_location()
 face_encoder = dlib.face_recognition_model_v1(face_recognition_model)
 
-#####################emotion####################################
 
 def eye_aspect_ratio(eye):
-	# compute the euclidean distances between the two sets of
-	# vertical eye landmarks (x, y)-coordinates
 	A = spatial.distance.euclidean(eye[1], eye[5])
 	B = spatial.distance.euclidean(eye[2], eye[4])
-
-	# compute the euclidean distance between the horizontal
-	# eye landmark (x, y)-coordinates
 	C = spatial.distance.euclidean(eye[0], eye[3])
-
-	# compute the eye aspect ratio
 	ear = (A + B) / (2.0 * C)
-
-	# return the eye aspect ratio
 	return ear
 
-#############################fr############################################################
 
 def _css_to_rect(css):
-    """
-    Convert a tuple in (top, right, bottom, left) order to a dlib `rect` object
-
-    :param css:  plain tuple representation of the rect in (top, right, bottom, left) order
-    :return: a dlib `rect` object
-    """
     return dlib.rectangle(css[3], css[0], css[1], css[2])
 
 def _rect_to_css(rect):
-    """
-    Convert a dlib 'rect' object to a plain tuple in (top, right, bottom, left) order
-
-    :param rect: a dlib 'rect' object
-    :return: a plain tuple representation of the rect in (top, right, bottom, left) order
-    """
     return rect.top(), rect.right(), rect.bottom(), rect.left()
 
 def _trim_css_to_bounds(css, image_shape):
-    """
-    Make sure a tuple in (top, right, bottom, left) order is within the bounds of the image.
-
-    :param css:  plain tuple representation of the rect in (top, right, bottom, left) order
-    :param image_shape: numpy shape of the image array
-    :return: a trimmed plain tuple representation of the rect in (top, right, bottom, left) order
-    """
     return max(css[0], 0), min(css[1], image_shape[1]), min(css[2], image_shape[0]), max(css[3], 0)
 
 def _raw_face_locations(img, number_of_times_to_upsample=1, model="hog"):
-    """
-    Returns an array of bounding boxes of human faces in a image
-
-    :param img: An image (as a numpy array)
-    :param number_of_times_to_upsample: How many times to upsample the image looking for faces. Higher numbers find smaller faces.
-    :param model: Which face detection model to use. "hog" is less accurate but faster on CPUs. "cnn" is a more accurate
-                  deep-learning model which is GPU/CUDA accelerated (if available). The default is "hog".
-    :return: A list of dlib 'rect' objects of found face locations
-    """
     if model == "cnn":
         return cnn_face_detector(img, number_of_times_to_upsample)
     else:
@@ -101,82 +62,32 @@ def _raw_face_landmarks(face_image, face_locations=None, model="large"):
     return [pose_predictor(face_image, face_location) for face_location in face_locations]
 
 def load_image_file(file, mode='RGB'):
-	"""
-	Loads an image file (.jpg, .png, etc) into a numpy array
-
-	:param file: image file name or file object to load
-	:param mode: format to convert the image to. Only 'RGB' (8-bit RGB, 3 channels) and 'L' (black and white) are supported.
-	:return: image contents as numpy array
-	"""
 	im = PIL.Image.open(file)
 	if mode:
 		im = im.convert(mode)
 	return np.array(im)
 
 def face_encodings(face_image, known_face_locations=None, num_jitters=1, model="small"):
-    """
-    Given an image, return the 128-dimension face encoding for each face in the image.
-
-    :param face_image: The image that contains one or more faces
-    :param known_face_locations: Optional - the bounding boxes of each face if you already know them.
-    :param num_jitters: How many times to re-sample the face when calculating encoding. Higher is more accurate, but slower (i.e. 100 is 100x slower)
-    :param model: Optional - which model to use. "large" or "small" (default) which only returns 5 points but is faster.
-    :return: A list of 128-dimensional face encodings (one for each face in the image)
-    """
     raw_landmarks = _raw_face_landmarks(face_image, known_face_locations, model)
     return [np.array(face_encoder.compute_face_descriptor(face_image, raw_landmark_set, num_jitters)) for raw_landmark_set in raw_landmarks]
 
 def face_locations(img, number_of_times_to_upsample=1, model="hog"):
-    """
-    Returns an array of bounding boxes of human faces in a image
-
-    :param img: An image (as a numpy array)
-    :param number_of_times_to_upsample: How many times to upsample the image looking for faces. Higher numbers find smaller faces.
-    :param model: Which face detection model to use. "hog" is less accurate but faster on CPUs. "cnn" is a more accurate
-                  deep-learning model which is GPU/CUDA accelerated (if available). The default is "hog".
-    :return: A list of tuples of found face locations in css (top, right, bottom, left) order
-    """
     if model == "cnn":
         return [_trim_css_to_bounds(_rect_to_css(face.rect), img.shape) for face in _raw_face_locations(img, number_of_times_to_upsample, "cnn")]
     else:
         return [_trim_css_to_bounds(_rect_to_css(face), img.shape) for face in _raw_face_locations(img, number_of_times_to_upsample, model)]
 
 def face_encodings(face_image, known_face_locations=None, num_jitters=1, model="small"):
-    """
-    Given an image, return the 128-dimension face encoding for each face in the image.
-
-    :param face_image: The image that contains one or more faces
-    :param known_face_locations: Optional - the bounding boxes of each face if you already know them.
-    :param num_jitters: How many times to re-sample the face when calculating encoding. Higher is more accurate, but slower (i.e. 100 is 100x slower)
-    :param model: Optional - which model to use. "large" or "small" (default) which only returns 5 points but is faster.
-    :return: A list of 128-dimensional face encodings (one for each face in the image)
-    """
     raw_landmarks = _raw_face_landmarks(face_image, known_face_locations, model)
     return [np.array(face_encoder.compute_face_descriptor(face_image, raw_landmark_set, num_jitters)) for raw_landmark_set in raw_landmarks]
 
 def face_distance(face_encodings, face_to_compare):
-    """
-    Given a list of face encodings, compare them to a known face encoding and get a euclidean distance
-    for each comparison face. The distance tells you how similar the faces are.
-
-    :param face_encodings: List of face encodings to compare
-    :param face_to_compare: A face encoding to compare against
-    :return: A numpy ndarray with the distance for each face in the same order as the 'faces' array
-    """
     if len(face_encodings) == 0:
         return np.empty((0))
 
     return np.linalg.norm(face_encodings - face_to_compare, axis=1)
 
 def compare_faces(known_face_encodings, face_encoding_to_check, tolerance=0.6):
-    """
-    Compare a list of face encodings against a candidate encoding to see if they match.
-
-    :param known_face_encodings: A list of known face encodings
-    :param face_encoding_to_check: A single face encoding to compare against the list
-    :param tolerance: How much distance between faces to consider it a match. Lower is more strict. 0.6 is typical best performance.
-    :return: A list of True/False values indicating which known_face_encodings match the face encoding to check
-    """
     return list(face_distance(known_face_encodings, face_encoding_to_check) <= tolerance)
 ########################################imutils###########################################################
 FACIAL_LANDMARKS_68_IDXS = OrderedDict([
@@ -192,44 +103,28 @@ FACIAL_LANDMARKS_68_IDXS = OrderedDict([
 FACIAL_LANDMARKS_IDXS = FACIAL_LANDMARKS_68_IDXS
 
 def shape_to_np(shape, dtype="int"):
-	# initialize the list of (x, y)-coordinates
 	coords = np.zeros((shape.num_parts, 2), dtype=dtype)
-
-	# loop over all facial landmarks and convert them
-	# to a 2-tuple of (x, y)-coordinates
 	for i in range(0, shape.num_parts):
 		coords[i] = (shape.part(i).x, shape.part(i).y)
-
-	# return the list of (x, y)-coordinates
 	return coords
 
 def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
-    # initialize the dimensions of the image to be resized and
-    # grab the image size
+
     dim = None
     (h, w) = image.shape[:2]
 
-    # if both the width and height are None, then return the
-    # original image
+
     if width is None and height is None:
         return image
 
-    # check to see if the width is None
     if width is None:
-        # calculate the ratio of the height and construct the
-        # dimensions
         r = height / float(h)
         dim = (int(w * r), height)
 
-    # otherwise, the height is None
     else:
-        # calculate the ratio of the width and construct the
-        # dimensions
         r = width / float(w)
         dim = (width, int(h * r))
 
-    # resize the image
     resized = cv2.resize(image, dim, interpolation=inter)
 
-    # return the resized image
     return resized
---
+++ 搬砖的key.py
@@ -0,0 +1,140 @@
+import api
+import cv2
+import numpy as np
+from keras.models import load_model
+import dlib
+import time
+import argparse
+import models
+from img import encodings
+
+obama_face_encoding = encodings(7)
+yu_encoding = encodings(1)
+hui_face_encoding = encodings(2)
+chen_face_encoding = encodings(3)
+rui_face_encoding = encodings(4)
+mu_face_encoding = encodings(5)
+man_face_encoding = encodings(6)
+
+known_face_encodings = [
+    obama_face_encoding,
+    yu_encoding,
+    hui_face_encoding,
+    chen_face_encoding,
+    rui_face_encoding,
+    mu_face_encoding,
+    man_face_encoding
+]
+
+known_face_names = [
+    "Handsome Boy",
+    "Yu",
+    "Hui",
+    "Chen",
+    "Rui",
+    "Mu",
+    "Man"
+]
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-t", "--threshold", type = float, default=0.20,
+	help="threshold to determine closed eyes")
+ap.add_argument("-f", "--frames", type = int, default=2,
+	help="the number of consecutive frames the eye must be below the threshold")
+args = vars(ap.parse_args())
+
+video_capture = cv2.VideoCapture(0)
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(models.pose_predictor_model_location())
+emotion_classifier = load_model(models.simple_CNN_530_0_65_hdf5())
+face_classifier = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+emotion=""
+trig = 0
+delay = 0
+ear = args['threshold']
+font = cv2.FONT_HERSHEY_DUPLEX
+(lStart, lEnd) = api.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = api.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+emotion_labels = {
+    0: 'angry',
+    1: 'disgust',
+    2: 'fear',
+    3: 'joy',
+    4: 'sad',
+    5: 'surprise',
+    6: 'clam'
+}
+
+while True:
+    ret, frame = video_capture.read()
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    rgb_small_frame = small_frame[:, :, ::-1]
+    if process_this_frame:
+        face_locations = []
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = api.resize(gray, width=450)
+        rects = detector(gray, 0)
+
+        for rect in rects:
+            shape = predictor(gray, rect)
+            shape = api.shape_to_np(shape)
+            leftEye = shape[lStart:lEnd]
+            rightEye = shape[rStart:rEnd]
+            leftEAR = api.eye_aspect_ratio(leftEye)
+            rightEAR = api.eye_aspect_ratio(rightEye)
+            ear = (leftEAR + rightEAR) / 2.0
+
+        if ear < args['threshold']:
+            trig = 1
+        if trig == 1:
+            delay += 1
+        if delay >= 6:
+            delay = 0
+            trig = 0
+            face_locations = api.face_locations(rgb_small_frame)
+            face_encodings = api.face_encodings(rgb_small_frame, face_locations)
+            faces = face_classifier.detectMultiScale(
+            gray, scaleFactor=1.2, minNeighbors=3, minSize=(40, 40))
+            for (x, y, w, h) in faces:
+                gray_face = gray[(y):(y + h), (x):(x + w)]
+                gray_face = cv2.resize(gray_face, (48, 48))
+                gray_face = gray_face / 255.0
+                gray_face = np.expand_dims(gray_face, 0)
+                gray_face = np.expand_dims(gray_face, -1)
+                emotion_label_arg = np.argmax(emotion_classifier.predict(gray_face))
+                emotion = emotion_labels[emotion_label_arg]
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                matches = api.compare_faces(known_face_encodings, face_encoding)
+                name = "Unknown"
+                face_distances = api.face_distance(known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
+                face_names.append(name)
+
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom + 20), (0, 0, 255), cv2.FILLED)
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        cv2.putText(frame, emotion, (left + 6, bottom + 16), font, 1.0, (255, 255, 255), 1)
+        time.sleep(1)
+    cv2.putText(frame, "Blink pls", (10, 30), font, 1.0, (0, 0, 255), 1)
+    cv2.imshow('Video', frame)            
+    process_this_frame = not process_this_frame
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
